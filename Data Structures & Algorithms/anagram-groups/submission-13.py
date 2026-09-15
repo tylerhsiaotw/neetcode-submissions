161b3f54eb26f35aec75956083905aa41ebca94f
Tyler <xiao.xiao.950212@gmail.com>
2026-09-15 22:49:09 +0800

    Add: anagram-groups - submission-13

---
+++ Data Structures & Algorithms/anagram-groups/submission-13.py
@@ -0,0 +1,14 @@
+import collections
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = collections.defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                i = ord(c) - ord('a')
+                count[i] += 1
+            anagram_map[tuple(count)].append(s)
+
+        return list(anagram_map.values())
+        
